49088b157345735c716970e1baf0ee1a476d484f
Aleksey <95738324+Shaulenkov@users.noreply.github.com>
2021-12-12 13:35:11 +0300

    Add files via upload

---
+++ lesson2/example1.cs
@@ -0,0 +1,54 @@
+﻿/*
+Написать метод, возвращающий минимальное из трех чисел.
+Шауленков Алексей
+*/
+
+ class Program
+    {
+
+        static int minOfThree (int a, int b, int c)
+        {
+            if (a >= b)
+            {
+                if (b >= c)
+                {
+                    return c;
+                }
+                else
+                {
+                    return b;
+                }
+
+            }
+            else
+            {
+                if (a >= c)
+                {
+                    return c;
+                }
+                else
+                {
+                    return a;
+                }
+            }
+        }
+
+     
+
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите третье число: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine( "Минимальным является: " + minOfThree(a,b,c));                   
+
+            Console.ReadKey();
+        }
+    }
