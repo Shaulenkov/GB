be044f7d53a9c68ab2c101021341aa953e68df5a
Aleksey <95738324+Shaulenkov@users.noreply.github.com>
2021-12-12 13:46:56 +0300

    Add files via upload

---
+++ lesson2/example3.cs
@@ -0,0 +1,38 @@
+﻿/*
+С клавиатуры вводятся числа, пока не будет введен 0. 
+Подсчитать сумму всех нечетных положительных чисел.
+Шауленков Алексей
+*/
+
+ class Program
+    {
+
+       
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите числа, для завершения введите 0: ");
+
+            int count = 0;
+            int Sum = 0;
+            while (true) 
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                {
+                    break;
+                }
+                else if(number > 0 && number % 2 != 0 )
+                {
+                    count++;
+                    Sum += number;
+                }
+            }
+            Console.WriteLine("Количество чисел: " + count);
+            Console.WriteLine("Сумма: " + Sum);
+
+            Console.ReadKey();
+        }
+    }
+        
